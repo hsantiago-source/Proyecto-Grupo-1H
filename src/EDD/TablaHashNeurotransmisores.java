b7c20822c278ee3f2f5825193147213dcc271353
Andres <benezra.andres@correo.unimet.edu.ve>
2026-06-03 18:06:23 -0400

    funcionalidades de hashing

--- src/EDD/TablaHashNeurotransmisores.java
+++ src/EDD/TablaHashNeurotransmisores.java
@@ -4,20 +4,24 @@
  */
 package EDD;
 
+import Entidades.Neurotransmisor;
+
 
 /**
  * 
  * @author hsantiago
  */
 public class TablaHashNeurotransmisores {
+    
     private NodoHash[] tabla;
     private int capacidad;
 
     public TablaHashNeurotransmisores(int capacidad) {
-        this.tabla = new NodoHash[capacidad];
         this.capacidad = capacidad;
+        this.tabla = new NodoHash[capacidad];
     }
 
+    // Getters y Setters
     public NodoHash[] getTabla() {
         return tabla;
     }
@@ -34,5 +38,46 @@ public class TablaHashNeurotransmisores {
         this.capacidad = capacidad;
     }
     
-    
+    private int funcionHash(String clave) {
+        int hash = 0;
+        for(int i = 0; i < clave.length(); i++) {
+            hash = (hash * 31 + clave.charAt(i)) % capacidad;
+        }
+        if (hash < 0) {
+            hash = hash * -1;
+        }
+        return hash;
+    }
+
+    public void insertar(String clave, Neurotransmisor quimico) {
+        int indice = funcionHash(clave);
+        NodoHash nuevoNodo = new NodoHash(clave, quimico, null);
+
+        if (tabla[indice] == null) {
+            tabla[indice] = nuevoNodo;
+        } else {
+            nuevoNodo.setSiguiente(tabla[indice]);
+            tabla[indice] = nuevoNodo;
+        }
+    }
+
+    public Neurotransmisor buscar(String clave) {
+        int indice = funcionHash(clave);
+        NodoHash actual = tabla[indice];
+
+        while (actual != null) {
+            if (actual.getClave().equals(clave)) {
+                return actual.getQuimico(); 
+            }
+            actual = actual.getSiguiente();
+        }
+        
+        return null; 
+    }
+
+    public void vaciarDiccionario() {
+        this.tabla = new NodoHash[this.capacidad];
+    }
+
+
 }
